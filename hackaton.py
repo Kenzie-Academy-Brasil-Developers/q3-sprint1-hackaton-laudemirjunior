ec13684852c946843ce5f44e5b046c0ef00239c2
Laudemir Nascimento <nlaudemir@gmail.com>
2021-12-06 12:15:35 -0300

    feat: finish homework

--- hackaton.py
+++ hackaton.py
@@ -1,1 +1,6 @@
-# Seu código aqui
+def get_score(team_name, teams):
+    result = 'Time não encontrado!'
+    for index, team in enumerate(teams):
+        if team == team_name:
+            result = f'A {team_name} ficou classificada em {index+1}'  
+    return result
